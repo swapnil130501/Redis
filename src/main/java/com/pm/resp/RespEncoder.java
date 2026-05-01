f19c4bc4b520407ffad9bb4a5f9bd30ac672a95c
swapnil <swapnil130501@gmail.com>
2026-05-01 14:57:40 +0530

    Add: integer resp encoder

--- src/main/java/com/pm/resp/RespEncoder.java
+++ src/main/java/com/pm/resp/RespEncoder.java
@@ -12,4 +12,8 @@ public class RespEncoder {
     public static byte[] bulkString(String s) {
         return ("$" + s.length() + "\r\n" + s + "\r\n").getBytes();
     }
+
+    public static byte[] integer(long n) {
+        return (":" + n + "\r\n").getBytes();
+    }
 }
